4aeebd8c427d7f4f0c9f64158ead044a60927343
chenwl <937096678@qq.com>
2021-11-05 09:19:13 +0800

    实现实时天气和未来几天天气功能

--- app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
+++ app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
@@ -3,7 +3,6 @@ package com.sunnyweather.android.logic.network
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.lang.RuntimeException
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
@@ -12,7 +11,15 @@ object SunnyWeatherNetwork {
 
     private val placeService = ServiceCreator.create<PlaceService>()
 
+    private val weatherService = ServiceCreator.create<WeatherService>()
+
     suspend fun searchPlaces(query: String) = placeService.searchPlaces(query).await()
+
+    suspend fun getDailyWeather(lng: String,lat: String) =
+            weatherService.getDailyWeather(lng,lat).await()
+
+    suspend fun getRealtimeWeather(lng: String,lat: String) =
+            weatherService.getRealtimeWeather(lng,lat).await()
 
     private suspend fun <T> Call<T>.await(): T {
         return suspendCoroutine { continuation ->
